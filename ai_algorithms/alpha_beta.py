bc0692e18824c7f596d8aa8491b18d496251f439
Sophia Cheto <sophiacheto@gmail.com>
2024-03-30 20:08:01 +0000

    alphabeta final version

--- ai_algorithms/alpha_beta.py
+++ ai_algorithms/alpha_beta.py
@@ -10,10 +10,10 @@ def alpha_beta(board: np.ndarray):
     best_move = -1
     best_score = float('-inf')
     for (child, col) in children:
-        if game.winning_move(child, c.AI_PIECE):
-            best_move = col
+        if game.winning_move(child, c.AI_PIECE):  # se alguma jogada já for vitoriosa, não calcula outras
+            best_move = col                                                   
             break
-        score = calcular(child, 1, float('-inf'), float('+inf'), depth_limit, False)
+        score = calculate(child, 1, float('-inf'), float('+inf'), depth_limit, False)
         if score > best_score:
             best_score = score
             best_move = col
@@ -22,17 +22,17 @@ def alpha_beta(board: np.ndarray):
 
 
 
-def calcular(board: np.ndarray, depth: int, alpha: int, beta: int, depth_limit: int, maximizing):
+def calculate(board: np.ndarray, depth: int, alpha: int, beta: int, depth_limit: int, maximizing):
     """Return the accumulated score for the current move"""
 
-    if depth == depth_limit or game.winning_move(board, 1) or game.winning_move(board, 2):
+    if depth == depth_limit or game.winning_move(board, 1) or game.winning_move(board, 2) or game.is_game_tied(board):
         return h.calculate_board_score(board, c.AI_PIECE, c.HUMAN_PIECE)
 
     if maximizing:
         maxEval = float('-inf')
         children = get_children(board, c.AI_PIECE)
-        for (child, col) in children:
-            eval = calcular(child, depth+1, alpha, beta, depth_limit, False)
+        for (child, _) in children:
+            eval = calculate(child, depth+1, alpha, beta, depth_limit, False)
             maxEval = max(maxEval, eval)
             alpha = max(alpha, eval)
             if beta <= alpha:
@@ -42,8 +42,8 @@ def calcular(board: np.ndarray, depth: int, alpha: int, beta: int, depth_limit: 
     else:
         minEval = float('+inf')
         children = get_children(board, c.HUMAN_PIECE)
-        for (child, col) in children:
-            eval = calcular(child, depth+1, alpha, beta, depth_limit, True)
+        for (child, _) in children:
+            eval = calculate(child, depth+1, alpha, beta, depth_limit, True)
             minEval = min(minEval, eval)
             beta = min(beta, eval)
             if beta <= alpha:
@@ -55,7 +55,7 @@ def calcular(board: np.ndarray, depth: int, alpha: int, beta: int, depth_limit: 
 def get_children(board, piece) -> None:
     """Return children of the actual state board"""
     children = []
-    if game.available_moves(board) == -1: return  
+    if game.available_moves(board) == -1: return children
     for col in game.available_moves(board):  
         copy_board = game.simulate_move(board, piece, col)   
         children.append((copy_board, col)) 
