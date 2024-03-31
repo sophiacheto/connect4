4ea39c274a8fea4b2103909bac9f139cd4fd105d
Sophia Cheto <sophiacheto@gmail.com>
2024-03-31 02:04:30 +0100

    fighting with mcts

--- ai_algorithms/mcts.py
+++ ai_algorithms/mcts.py
@@ -2,6 +2,7 @@ import time, math, numpy as np, random, itertools
 from game_rules import constants as c
 from game_rules import game_logic as game
 from math import sqrt, log
+from ai_algorithms import a_star as a
 
 
 class Node:
@@ -12,6 +13,13 @@ class Node:
         self.visits = 0
         self.wins = 0  
         self.current_player = 1 if last_player == 2 else 2
+
+    def __str__(self) -> str:
+        string = "Vitórias: " + str(self.wins) + '\n'
+        string += "Total: " + str(self.visits) + '\n'
+        string += "Pontuação: " + str(self.ucb()) + '\n'
+        string += "Probabilidade de vitória: " + str(self.score()) + '\n'
+        return string
 
     def add_children(self) -> None:
         """add each possible move to a list of possible children for the current node/state"""
@@ -88,6 +96,11 @@ class MCTS:
         board = node.board.copy()   # cria uma cópia do tabuleiro do nó para ser alterado
         players = itertools.cycle([c.AI_PIECE, c.HUMAN_PIECE])  # cria uma iteração sobre os jogadores de cada nível
         current_player = next(players)
+        
+        current_player = next(players)
+        col = a.a_star_adversarial(board, current_player, next(players))
+        board = game.simulate_move(board, current_player, col)
+        
         while not (game.winning_move(board, c.AI_PIECE) or game.winning_move(board, c.HUMAN_PIECE)):   # continua a simulação até o jogo simulado acabar
             current_player = next(players)
             values = game.available_moves(board)   # seleciona as colunas disponpiveis a receberem jogadas
@@ -98,6 +111,21 @@ class MCTS:
             board = game.simulate_move(board, current_player, col)  # simula a jogada escolhida
         return current_player       # retorna o jogador da jogada vencedora (ou seja, quem ganhou a simulação)
     
+        # """simulate a entire play until someone wins"""
+        # board = node.board.copy()   # cria uma cópia do tabuleiro do nó para ser alterado
+        # players = itertools.cycle([c.AI_PIECE, c.HUMAN_PIECE])  # cria uma iteração sobre os jogadores de cada nível
+        # current_player = next(players)
+        # while not (game.winning_move(board, c.AI_PIECE) or game.winning_move(board, c.HUMAN_PIECE)):   # continua a simulação até o jogo simulado acabar
+        #     if game.is_game_tied(board): return 0
+        #     current_player = next(players)
+        #     values = game.available_moves(board)   # seleciona as colunas disponpiveis a receberem jogadas
+        #     if values == -1:       # se não houver possibilidades de jogadas, retorna que não houve ganhador (empate)
+        #         current_player = 0
+        #         break
+        #     col = random.choice(values)    # escolhe aleatoriamente uma das possibilidades de jogada
+        #     board = game.simulate_move(board, current_player, col)  # simula a jogada escolhida
+        # return current_player       # retorna o jogador da jogada vencedora (ou seja, quem ganhou a simulação)
+    
     def best_move(self) -> int:
         """select the best column to be played based on their scores"""
         max_uct = float('-inf')
@@ -105,6 +133,8 @@ class MCTS:
         columns = []   # armazena as colunas que têm o melhor score de vitórias
         for (child, col) in self.root.children:   # para cada possível jogada...
             uct = child.score()      # calcula o score do filho
+            print(f"Coluna: {col}")
+            print(child)
             if uct > max_uct:        
                 max_uct = uct        # se esse for o novo melhor score, armazena como mehor
             scores[col] = uct        # ?????????????????????????????????????????????????????????????
